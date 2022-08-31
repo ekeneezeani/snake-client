98c04a28b280761e6b597e441154472535a25e9c
ekeneezeani <108833282+ekeneezeani@users.noreply.github.com>
2022-08-31 15:07:54 -0500

    implemented snake movement up only

--- client.js
+++ client.js
@@ -7,15 +7,18 @@ const config = {
 
 const connect = function () {
   const conn = net.createConnection(config);
-  conn.on('data', (data) =>{
-    console.log('data from server  ', data);
+  conn.on("data", (data) => {
+    console.log("data from server  ", data);
   });
-  conn.on('connect',()=>{
-    conn.write('Name: EES')
-    console.log("Successfully connected to the server")
-  })
-  conn.setEncoding("utf8");
-  return conn;
+  conn.on("connect", () => {
+    conn.write('Name: EES');
+    console.log("Successfully connected to the server");
+    setInterval(() => {
+      conn.write('Move: up');
+    },1000);
+    conn.setEncoding("utf8");
+    return conn;
+  });
 };
 
-module.exports = {connect};
+module.exports = { connect };
